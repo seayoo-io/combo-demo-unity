2594e05d007075a29db6cd9b1d4c717f93dc0160
LRYuuu <liruoyu@kingsoft.com>
2024-03-28 18:29:28 +0800

    添加游戏背景音乐 (#3)

---
+++ Assets/Scripts/Components/BackgroundSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BackgroundSound : MonoBehaviour
+{
+    private static BackgroundSound _instance;
+    void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
