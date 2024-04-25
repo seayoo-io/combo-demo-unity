ad827ba593fc652536830e45874c52ff9e3ef752
liruoyu <liruoyu@kingsoft.com>
2024-04-25 14:16:55 +0800

    新增 OpenGameUrl 功能 (#29)

--- Assets/Scripts/Components/Views/SettingView.cs
+++ Assets/Scripts/Components/Views/SettingView.cs
@@ -11,16 +11,31 @@ internal class SettingView : View<SettingView>
     public Button clearBtn;
     public Button cancelBtn;
     public Button appSettingsBtn;
+    public Button playerAgreementBtn;
+    public Button privacyPolicyBtn;
+    public Button privacyChildrenBtn;
+    public Button thirdPartyBtn;
+    public Button fangchenmiBtn;
     private Action OnLogout;
     private Action OnClear;
     private Action OnCancel; 
     private Action OnAppSettings;
+    private Action OnPlayerAgreement;
+    private Action OnPrivacyPolicy;
+    private Action OnPrivacyChildren;
+    private Action OnThirdParty;
+    private Action OnFangchenmi;
     void Awake()
     {
         logoutBtn.onClick.AddListener(OnLogoutConfirmBtn);
         clearBtn.onClick.AddListener(OnClearConfirmBtn);
         cancelBtn.onClick.AddListener(OnClickCancelBtn);
         appSettingsBtn.onClick.AddListener(OnAppSettingsBtn);
+        playerAgreementBtn.onClick.AddListener(OnPlayerAgreementBtn);
+        privacyPolicyBtn.onClick.AddListener(OnPrivacyPolicyBtn);
+        privacyChildrenBtn.onClick.AddListener(OnPrivacyChildrenBtn);
+        thirdPartyBtn.onClick.AddListener(OnThirdPartyBtn);
+        fangchenmiBtn.onClick.AddListener(OnFangchenmiBtn);
         ButtonManager.SetButtonEnabledByType(logoutBtn, ButtonType.LogoutButton);
         ButtonManager.SetButtonEnabledByType(clearBtn, ButtonType.ClearButton);
     }
@@ -31,6 +46,11 @@ internal class SettingView : View<SettingView>
         clearBtn.onClick.RemoveListener(OnClearConfirmBtn);
         cancelBtn.onClick.RemoveListener(OnClickCancelBtn);
         appSettingsBtn.onClick.RemoveListener(OnAppSettingsBtn);
+        playerAgreementBtn.onClick.RemoveListener(OnPlayerAgreementBtn);
+        privacyPolicyBtn.onClick.RemoveListener(OnPrivacyPolicyBtn);
+        privacyChildrenBtn.onClick.RemoveListener(OnPrivacyChildrenBtn);
+        thirdPartyBtn.onClick.RemoveListener(OnThirdPartyBtn);
+        fangchenmiBtn.onClick.RemoveListener(OnFangchenmiBtn);
     }
 
     void OnLogoutConfirmBtn()
@@ -53,6 +73,31 @@ internal class SettingView : View<SettingView>
         OnAppSettings.Invoke();
     }
 
+    void OnPlayerAgreementBtn()
+    {
+        OnPlayerAgreement.Invoke();
+    }
+
+    void OnPrivacyPolicyBtn()
+    {
+        OnPrivacyPolicy.Invoke();
+    }
+
+    void OnPrivacyChildrenBtn()
+    {
+        OnPrivacyChildren.Invoke();
+    }
+
+    void OnThirdPartyBtn()
+    {
+        OnThirdParty.Invoke();
+    }
+
+    void OnFangchenmiBtn()
+    {
+        OnFangchenmi.Invoke();
+    }
+
     public void SetLogoutCallback(Action OnLogout)
     {
         this.OnLogout = OnLogout;
@@ -71,6 +116,27 @@ internal class SettingView : View<SettingView>
     public void SetAppSettingsCallback(Action OnAppSettings)
     {
         this.OnAppSettings = OnAppSettings;
+    }
+
+    public void SetPlayerAgreementCallback(Action OnPlayerAgreement)
+    {
+        this.OnPlayerAgreement = OnPlayerAgreement;
+    }
+    public void SetPrivacyPolicyCallback(Action OnPrivacyPolicy)
+    {
+        this.OnPrivacyPolicy = OnPrivacyPolicy;
+    }
+    public void SetPrivacyChildrenCallback(Action OnPrivacyChildren)
+    {
+        this.OnPrivacyChildren = OnPrivacyChildren;
+    }
+    public void SetThirdPartyCallback(Action OnThirdParty)
+    {
+        this.OnThirdParty = OnThirdParty;
+    }
+    public void SetFangchenmiCallback(Action OnFangchenmi)
+    {
+        this.OnFangchenmi = OnFangchenmi;
     }
 
     protected override IEnumerator OnHide()
