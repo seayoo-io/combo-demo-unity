94b4162fc1a1de6c20bcc7a57c774177624bdecb
liruoyu <liruoyu@kingsoft.com>
2024-05-08 18:49:40 +0800

    新增 Debug 信息

--- Assets/Scripts/Components/Controllers/ProductManager.cs
+++ Assets/Scripts/Components/Controllers/ProductManager.cs
@@ -46,11 +46,13 @@ public class ProductManager : MonoBehaviour
         view.SetProductName(productName);
         view.SetProductPrice(productPrice);
         view.gameObject.transform.SetParent(parentTransform, false);
+        Log.I(limitProducts);
         if(Array.Exists(limitProducts, limitProductId => limitProductId == productId))
         {
+            Log.I("包含");
             view.SetLimitProductText();
         }
-
+        Log.I("显示图片");
         if (!string.IsNullOrEmpty(productImageUrl)) {
             GameClient.GetProductImg(
                 productImageUrl,
