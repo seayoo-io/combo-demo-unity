fcf30235b1c350f25696564068f96ed40329b940
liruoyu <liruoyu@kingsoft.com>
2025-01-08 16:34:47 +0800

    将时区改成本地时区

--- Assets/Scripts/Components/Controllers/ReportDataManager.cs
+++ Assets/Scripts/Components/Controllers/ReportDataManager.cs
@@ -122,26 +122,7 @@ public class ReportDataManager : MonoBehaviour
         DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(unixTime);
 
         // 定义一个时区信息变量
-        TimeZoneInfo chinaTimeZone = null;
-
-        try
-        {
-            // 尝试获取 "China Standard Time" 时区信息
-            chinaTimeZone = TimeZoneInfo.FindSystemTimeZoneById("China Standard Time");
-        }
-        catch (TimeZoneNotFoundException)
-        {
-            try
-            {
-                // 如果找不到，尝试获取 "Asia/Shanghai" 时区信息
-                chinaTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Asia/Shanghai");
-            }
-            catch (TimeZoneNotFoundException)
-            {
-                // 如果仍然找不到，抛出一个自定义异常或使用本地时区作为替代
-                chinaTimeZone = TimeZoneInfo.Local;
-            }
-        }
+        TimeZoneInfo chinaTimeZone = TimeZoneInfo.Local;
 
         // 转换为目标时区的时间
         DateTimeOffset chinaTime = TimeZoneInfo.ConvertTime(dateTimeOffset, chinaTimeZone);
