47dc8dd2f04851eb6ff94db19187cb3f3af1e32e
liruoyu <liruoyu@kingsoft.com>
2025-08-19 17:02:43 +0800

    ci: Android 和 iOS 平台打包时新增横竖屏选项 (#110)

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -93,6 +93,15 @@ public class Builder : EditorWindow
 
     static void BuildAndroidDemo()
     {
+        switch (Environment.GetEnvironmentVariable("SCREEN_ORIENTATION"))
+        {
+            case "Landscape":
+                PlayerSettings.defaultInterfaceOrientation = UIOrientation.LandscapeLeft;
+                break;
+            case "Portrait":
+                PlayerSettings.defaultInterfaceOrientation = UIOrientation.Portrait;
+                break;
+        }
         var exportPath = Environment.GetEnvironmentVariable("EXPORT_PATH");
         PlayerSettings.bundleVersion = Environment.GetEnvironmentVariable("BUNDLE_VERSION");
         PlayerSettings.productName = Environment.GetEnvironmentVariable("COMBOSDK_GAME_ID")?? "demo";
@@ -119,6 +128,15 @@ public class Builder : EditorWindow
 
     static void BuildIOSDemo()
     {
+        switch (Environment.GetEnvironmentVariable("SCREEN_ORIENTATION"))
+        {
+            case "Landscape":
+                PlayerSettings.defaultInterfaceOrientation = UIOrientation.LandscapeLeft;
+                break;
+            case "Portrait":
+                PlayerSettings.defaultInterfaceOrientation = UIOrientation.Portrait;
+                break;
+        }
         var exportPath = Environment.GetEnvironmentVariable("EXPORT_PATH");
         PlayerSettings.bundleVersion = Environment.GetEnvironmentVariable("BUNDLE_VERSION");
         CreateDir(exportPath);
